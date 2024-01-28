8d390072836a3a2dde843b4782ad1a27d590207c
Hemant kumar <baghelhemant610@gmail.com>
2024-01-28 23:06:03 +0530

    learning some commands

--- car.java
+++ car.java
@@ -1,5 +1,7 @@
 public class car {
     public static void main(String[] args) {
         System.out.println("helo this is my car");
+        System.out.println("lets learn about the github")
+        System.out.println("lets learn some commands")
     }
 }
